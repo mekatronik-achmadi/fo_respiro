876d65fcea94ac7703e6c919097bdccdf4c19fcc
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2018-10-09 15:26:22 +0700

    android graphlib

--- android/Test/src/com/example/Test/TestActivity.java
+++ android/Test/src/com/example/Test/TestActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.softmoore.android.graphlib.Function;
+import com.softmoore.android.graphlib.Graph;
+import com.softmoore.android.graphlib.GraphView;
+
 public class TestActivity extends Activity {
     /**
      * Called when the activity is first created.
@@ -22,18 +26,32 @@ public class TestActivity extends Activity {
         toast.show();
     }
 
+    Function xkuadrat = new Function() {
+        @Override
+        public double apply(double v) {
+            return v*v;
+        }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         Button btnPesan = (Button) findViewById(R.id.btnPesan);
-
         btnPesan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Pesan("I'm in Love (Fake Love)");
             }
         });
+
+        Graph graf = new Graph.Builder()
+                .addFunction(xkuadrat)
+                .setAxes(0,150)
+                .build();
+        GraphView grafview = (GraphView) findViewById(R.id.grafview);
+        grafview.setGraph(graf);
+
     }
 }
