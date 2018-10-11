c73d3b67c1326fcf83f2ad3669cd7cfd772796ac
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2018-10-11 20:12:06 +0700

    Android HC-05 Terminal works

--- android/Test/src/com/example/Test/TestActivity.java
+++ android/Test/src/com/example/Test/TestActivity.java
@@ -6,7 +6,11 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +18,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
 public class TestActivity extends Activity {
@@ -22,22 +28,70 @@ public class TestActivity extends Activity {
     /*
     Deklarasi variabel dan objek untuk satu class
      */
+
     // Bluetooth related
-    private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter btAdapter=null;
     private BluetoothSocket btSocket=null;
-    private OutputStream outStream=null;
+    private StringBuilder sb = new StringBuilder();
 
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private static String addressbt = "00:00:00:00:00:00";
+    private static String addressbt = "98:D3:32:21:44:6E";
+
+    private ConnectedThread mConnectedThread;
+    final int RECIEVE_MESSAGE = 1;
+    Handler hdl;
 
     // Widget
     TextView txtOut;
     Button btnConnect;
 
     /*
-    Rutin tambahan
+    Aktifitas Intent Utama
      */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //Widget Creation
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+        txtOut = (TextView) findViewById(R.id.txtOut);
+        txtOut.setMovementMethod(new ScrollingMovementMethod());
+
+        hdl = new Handler(){
+            public void handleMessage(Message msg){
+                switch (msg.what){
+                    case RECIEVE_MESSAGE:
+                        byte[] readbuf = (byte[]) msg.obj;
+                        String strInComm = new String(readbuf,0,msg.arg1);
+                        sb.append(strInComm);
+
+                        int eolIndex = sb.indexOf("\n\r");
+                        if(eolIndex > 0) {
+                            String sbprint = sb.substring(0,eolIndex);
+                            sb.delete(0,sb.length());
+                            txtOut.append(sbprint + "\n\r");
+                        }
+                        break;
+                }
+            }
+        };
+
+        //Bluetooth Creation
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        CheckStateBT();
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectBT();
+            }
+        });
+    }
+
+    /*
+    Rutin tambahan
+    */
     private void Pesan(String txtpesan){
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
@@ -48,17 +102,27 @@ public class TestActivity extends Activity {
         toast.show();
     }
 
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice dev) throws IOException{
+        if(Build.VERSION.SDK_INT >= 10){
+            try{
+                final Method m = dev.getClass().getMethod("createInsecureRfcommSocketToServiceRecord",new Class[] {UUID.class});
+                return (BluetoothSocket) m.invoke(dev,MY_UUID);
+            }catch (Exception e){
+                String pesanerror = "Could not create Insecure RFComm Connection" + e.getMessage() + ".";
+                Pesan(pesanerror);
+            }
+        }
+        return dev.createRfcommSocketToServiceRecord(MY_UUID);
+    }
+
     private void CheckStateBT(){
         if(btAdapter==null){
             Pesan("Fatal Error: Bluetooth Not supported. Aborting.");
         }
         else{
-            if (btAdapter.isEnabled()){
-                txtOut.append("\n...Bluetooth is enabled...");
-            }
-            else{
+            if (!btAdapter.isEnabled()){
                 Intent enablingBT = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enablingBT,REQUEST_ENABLE_BT);
+                startActivityForResult(enablingBT,1);
             }
         }
     }
@@ -66,9 +130,8 @@ public class TestActivity extends Activity {
     private void ConnectBT(){
         BluetoothDevice devBT = btAdapter.getRemoteDevice(addressbt);
 
-        txtOut.append("\n...connecting to target...");
         try{
-            btSocket = devBT.createRfcommSocketToServiceRecord(MY_UUID);
+            btSocket = createBluetoothSocket(devBT);
         }catch (IOException e){
             String pesanerror = "Fatal Error: socket creation failed: " + e.getMessage() + ".";
             Pesan(pesanerror);
@@ -90,43 +153,55 @@ public class TestActivity extends Activity {
             }
         }
 
-        try{
-            outStream = btSocket.getOutputStream();
-        }catch(IOException e){
-            String pesanerror = "Fatal Error: out stream creation failed: " + e.getMessage() + ".";
-            Pesan(pesanerror);
+        btnConnect.setEnabled(false);
+
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+    }
+
+    /*
+    Thread tambahan
+     */
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket){
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try{
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            }catch(IOException e){}
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run(){
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            while(true){
+                try{
+                    bytes = mmInStream.read(buffer);
+                    hdl.obtainMessage(RECIEVE_MESSAGE,bytes,-1,buffer).sendToTarget();
+                }catch (IOException e){
+                    break;
+                }
+            }
+        }
+
+        public void write(String msg){
+            byte[] msgbuffer = msg.getBytes();
+            try{
+                mmOutStream.write(msgbuffer);
+            }catch(IOException e){}
         }
 
     }
 
-    /*
-    Aktifitas Intent
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        //Widget Creation
-        txtOut = (TextView) findViewById(R.id.txtOut);
-        btnConnect = (Button) findViewById(R.id.btnConnect);
-
-        //Bluetooth Creation
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        CheckStateBT();
-
-        btnConnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ConnectBT();
-            }
-        });
-    }
-
-    public void onStart(){
-        super.onStart();
-        txtOut.append("\n...In onStart()...");
-    }
-
-
+    //END of One Big Main Class
 }
