3df5264ace45d448df1d17ec8d7f9194808b5c79
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2018-10-30 12:10:40 +0700

    doxygen document related

--- firmware/cpu_joinwit/main/fo_adc.h
+++ firmware/cpu_joinwit/main/fo_adc.h
@@ -10,9 +10,23 @@
 #include "stdio.h"
 #include "string.h"
 
+/**
+ * @brief   define ADC channel.
+ */
+#if !defined(ADC_GRP1_NUM_CHANNELS) || defined(__DOXYGEN__)
 #define ADC_GRP1_NUM_CHANNELS   1
-#define ADC_GRP1_BUF_DEPTH      100
+#endif
 
+/**
+ * @brief   define ADC matrix depth.
+ */
+#if !defined(#define ADC_GRP1_BUF_DEPTH) || defined(__DOXYGEN__)
+#define ADC_GRP1_BUF_DEPTH      100
+#endif
+
+/**
+ * @brief   Starting ADC routine.
+ */
 void start_adc(void);
 
 #endif // FO_ADC_H
